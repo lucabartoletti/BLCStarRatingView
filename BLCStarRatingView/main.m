a4ad6183cc8226f2219a26c0d93f03df9739fb1b
Luca Bartoletti <luca.bartoletti@busuu.com>
2014-04-16 22:11:24 +0100

    Initial Commit

---
+++ BLCStarRatingView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BLCStarRatingView
+//
+//  Created by Luca Bartoletti on 16/04/2014.
+//  Copyright (c) 2014 Luca Bartoletti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ BLCStarRatingViewTests/BLCStarRatingViewTests.m
@@ -0,0 +1,34 @@
+//
+//  BLCStarRatingViewTests.m
+//  BLCStarRatingViewTests
+//
+//  Created by Luca Bartoletti on 16/04/2014.
+//  Copyright (c) 2014 Luca Bartoletti. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BLCStarRatingViewTests : XCTestCase
+
+@end
+
+@implementation BLCStarRatingViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
