c014085584512af81e142c1a25808554ecf3fe73
Luca Bartoletti <luca.bartoletti@busuu.com>
2014-07-31 01:00:07 +0100

    Update the documention

--- BLCStarRatingView/BLCStarRatingView.h
+++ BLCStarRatingView/BLCStarRatingView.h
@@ -14,10 +14,26 @@
 
 @optional
 
+/**
+ *  Called when the user start an update phase
+ *
+ *  @param ratingView The rating view in which the update occurs
+ */
 - (void)starRatingViewUpdateBegan:(BLCStarRatingView*)ratingView;
 
+/**
+ *  Called when the value of the rating is updated
+ *
+ *  @param ratingView The rating view in which the update occurs
+ *  @param value The update value of the rating
+ */
 - (void)starRatingView:(BLCStarRatingView*)starRatingView rateDidChange:(NSUInteger)value;
 
+/**
+ *  Called when the user ends an update phase
+ *
+ *  @param ratingView The rating view in which the update occurred
+ */
 - (void)starRatingViewUpdateEnded:(BLCStarRatingView*)ratingView;
 
 @end
@@ -29,10 +45,13 @@
  */
 @property (assign, nonatomic) NSUInteger rating;
 
+/**
+ *  The delegate
+ */
 @property (weak, nonatomic) id<BLCStarRatingViewDelegate> delegate;
 
 /**
- *  The horizontal space between stars. Default is 5
+ *  The horizontal space between stars. Default is 5.0f
  */
 @property (assign, nonatomic) CGFloat starHorizontalSpace;
 
